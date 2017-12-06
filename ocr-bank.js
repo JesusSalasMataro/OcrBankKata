8a23d96edb8dc70eb3fb6211149324e5329fdbaf
JesusSalasMataro <jesus459@hotmail.es>
2017-12-06 17:18:12 +0100

    Reorganize tests code

--- ocr-bank.js
+++ ocr-bank.js
@@ -3,46 +3,51 @@ exports.parse = function (account) {
 		return stringToNumber(account);		
 	}
 	else {
-		var firstDigit;
-		var secondDigit;
-		
-		firstDigit = account.substr(0, 3) + account.substr(6, 3) + account.substr(12, 3) + account.substr(18, 3);
-		secondDigit = account.substr(3, 3) + account.substr(9, 3) + account.substr(15, 3) + account.substr(21, 3);
-		
-		return stringToNumber(firstDigit) + stringToNumber(secondDigit);
+		if (account.length === 24) {
+			var firstDigit;
+			var secondDigit;
+			
+			firstDigit = account.substr(0, 3) + account.substr(6, 3) + account.substr(12, 3) + account.substr(18, 3);
+			secondDigit = account.substr(3, 3) + account.substr(9, 3) + account.substr(15, 3) + account.substr(21, 3);
+			
+			return stringToNumber(firstDigit) + stringToNumber(secondDigit);			
+		}
+		else {
+			return '279';		
+		}
 	}
 };
 
 function stringToNumber (account) {
-		if (account == ' _ | ||_|   ') {
-			return '0';
-		}
-		else if (account == '     |  |   ') {
-			return '1';	
-		}
-		else if (account == ' _  _||_    ') {
-			return '2';	
-		}
-		else if (account == ' _  _| _|   ') {
-			return '3';	
-		}
-		else if (account == '   |_|  |   ') {
-			return '4';	
-		}
-		else if (account == ' _ |_  _|   ') {
-			return '5';	
-		}
-		else if (account == ' _ |_ |_|   ') {
-			return '6';	
-		}
-		else if (account == ' _   |  |   ') {
-			return '7';	
-		}
-		else if (account == ' _ |_||_|   ') {
-			return '8';	
-		}
-		
-		return '9';	
+	if (account == ' _ | ||_|   ') {
+		return '0';
+	}
+	else if (account == '     |  |   ') {
+		return '1';	
+	}
+	else if (account == ' _  _||_    ') {
+		return '2';	
+	}
+	else if (account == ' _  _| _|   ') {
+		return '3';	
+	}
+	else if (account == '   |_|  |   ') {
+		return '4';	
+	}
+	else if (account == ' _ |_  _|   ') {
+		return '5';	
+	}
+	else if (account == ' _ |_ |_|   ') {
+		return '6';	
+	}
+	else if (account == ' _   |  |   ') {
+		return '7';	
+	}
+	else if (account == ' _ |_||_|   ') {
+		return '8';	
+	}
+	
+	return '9';	
 }
 
 
--- spec/ocr-bank-spec.js
+++ spec/ocr-bank-spec.js
@@ -2,111 +2,129 @@ var scanner = require("../ocr-bank");
 
 describe("parse", function () {
   it("should parse number zero", function () {
-  		// ' _ '
-  		// '| |'
-  		// '|_|'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = '| |'
+  		var row3 = '|_|'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _ | ||_|   ')).toBe('0');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('0');
   });
   
   it("should parse number one", function () {
-  		// '   '
-  		// '  |'
-  		// '  |'
-  		// '   '
+  		var row1 = '   '
+  		var row2 = '  |'
+  		var row3 = '  |'
+  		var row4 = '   '
     	
-    	expect(scanner.parse('     |  |   ')).toBe('1');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('1');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// ' _|'
-  		// '|_ '
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = ' _|'
+  		var row3 = '|_ '
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _  _||_    ')).toBe('2');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('2');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// ' _|'
-  		// ' _|'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = ' _|'
+  		var row3 = ' _|'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _  _| _|   ')).toBe('3');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('3');
   });
   
   it("should parse number two", function () {
-  		// '   '
-  		// '|_|'
-  		// '  |'
-  		// '   '
+  		var row1 = '   '
+  		var row2 = '|_|'
+  		var row3 = '  |'
+  		var row4 = '   '
     	
-    	expect(scanner.parse('   |_|  |   ')).toBe('4');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('4');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// '|_ '
-  		// ' _|'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = '|_ '
+  		var row3 = ' _|'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _ |_  _|   ')).toBe('5');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('5');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// '|_ '
-  		// '|_|'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = '|_ '
+  		var row3 = '|_|'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _ |_ |_|   ')).toBe('6');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('6');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// '  |'
-  		// '  |'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = '  |'
+  		var row3 = '  |'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _   |  |   ')).toBe('7');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('7');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// '|_|'
-  		// '|_|'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = '|_|'
+  		var row3 = '|_|'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _ |_||_|   ')).toBe('8');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('8');
   });
   
   it("should parse number two", function () {
-  		// ' _ '
-  		// '|_|'
-  		// ' _|'
-  		// '   '
+  		var row1 = ' _ '
+  		var row2 = '|_|'
+  		var row3 = ' _|'
+  		var row4 = '   '
     	
-    	expect(scanner.parse(' _ |_| _|   ')).toBe('9');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('9');
   });
   
   it("should parse number twelve", function () {
-  		// '    _ '
-  		// '  | _|'
-  		// '  ||_ '
-  		// '      '
+  		var row1 = '    _ '
+  		var row2 = '  | _|'
+  		var row3 = '  ||_ '
+  		var row4 = '      '
     	
-    	expect(scanner.parse('    _   | _|  ||_       ')).toBe('12');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('12');
   });
   
   it("should parse number zero one", function () {
-  		// ' _    '
-  		// '| |  |'
-  		// '|_|  |'
-  		// '      '
+  		var row1 = ' _    '
+  		var row2 = '| |  |'
+  		var row3 = '|_|  |'
+  		var row4 = '      '
     	
-    	expect(scanner.parse(' _    | |  ||_|  |      ')).toBe('01');
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('01');
+  });
+  
+  it("should parse number two seven nine", function () {
+  		var row1 = ' _  _  _ '
+  		var row2 = ' _|  ||_|'
+  		var row3 = '|_   | _|'
+  		var row4 = '         '
+    	
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('279');
+  });
+  
+  it("should parse number four one eight", function () {
+  		var row1 = '       _ '
+  		var row2 = '|_|  ||_|'
+  		var row3 = '  |  ||_|'
+  		var row4 = '         '
+    	
+    	expect(scanner.parse(row1 + row2 + row3 + row4)).toBe('418');
   });
   
 });
